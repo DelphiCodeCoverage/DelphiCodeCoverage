4a458489eb4a68b719c4265d026c7b5509b361cb
ekot1 <e.kotlyarov@gmail.com>
2018-05-29 12:49:45 -0700

    Updated license to MPL 2.0

--- Source/EmmaDataFile.pas
+++ Source/EmmaDataFile.pas
@@ -1,11 +1,12 @@
-(* ************************************************************ *)
-(* Delphi Code Coverage *)
-(* *)
-(* A quick hack of a Code Coverage Tool for Delphi 2010 *)
-(* by Christer Fahlgren and Nick Ring *)
-(* ************************************************************ *)
-(* Licensed under Mozilla Public License 1.1 *)
-(* ************************************************************ *)
+(***********************************************************************)
+(* Delphi Code Coverage                                                *)
+(*                                                                     *)
+(* A quick hack of a Code Coverage Tool for Delphi                     *)
+(* by Christer Fahlgren and Nick Ring                                  *)
+(*                                                                     *) 
+(* This Source Code Form is subject to the terms of the Mozilla Public *)
+(* License, v. 2.0. If a copy of the MPL was not distributed with this *)
+(* file, You can obtain one at http://mozilla.org/MPL/2.0/.            *)
 
 unit EmmaDataFile;
 
